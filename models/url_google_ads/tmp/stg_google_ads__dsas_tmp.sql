68f20c3000800153e334741e34480df66592cd59
bnealdefero <bneal@deferousa.com>
2021-12-05 08:38:27 -0700

    running an anti-join to get rows that exist in the campaign level spend view that do not exist in the google_ads_url_ad_adapter

---
+++ models/url_google_ads/tmp/stg_google_ads__dsas_tmp.sql
@@ -0,0 +1,23 @@
+with staged as (
+    -- this contains ALL campaigns
+    select * from {{ ref('stg_google_ads__campaign_spend_tmp') }}
+),
+adapter as (
+    -- this contains ALL campaigns MINUS dsa's since it's built off of ad_stats
+    select * from {{ ref('google_ads__url_ad_adapter')}}
+),
+joined as (
+    -- get rows that exist in staged that do not exist in adapter
+    SELECT
+        a.*
+    FROM
+        staged a
+        LEFT JOIN 
+        adapter b 
+        ON a.campaign_id = b.campaign_id
+    WHERE
+        b.campaign_id IS NULL
+)
+
+select * from joined
+
