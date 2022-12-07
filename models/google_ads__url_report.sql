21e32c9426e191da6bb84e8cc807d56f433fd429
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2022-12-07 12:10:16 -0600

    feature/enable-null-urls

--- models/google_ads__url_report.sql
+++ models/google_ads__url_report.sql
@@ -83,8 +83,14 @@ fields as (
     left join accounts
         on campaigns.account_id = accounts.account_id
 
-    -- We only want utm ads to populate this report. Therefore, we filter where url ads are populated.
-    where ads.source_final_urls is not null
+    {% if   (var('enable_google_ads_null_urls', False)) or
+            (var('enable_ad_reporting_null_urls', False))  %}
+        -- In this case, skip where clause to include all rows whether or not the url field is populated.
+    {% else %}
+        -- We only want utm ads to populate this report. Therefore, we filter where url ads are populated.
+        where ads.source_final_urls is not null
+    {% endif %}
+
     {{ dbt_utils.group_by(16) }}
 )
 
